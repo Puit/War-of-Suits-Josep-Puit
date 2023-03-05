9dc1389660f618b2de190384ca1f7ce9161146c3
Puit <joseppuit@gmail.com>
2023-03-05 19:19:05 +0100

    Create Scorboard component

---
+++ app/src/main/java/com/nunnos/warofsuitsjoseppuit/presentation/components/Scoreboard.kt
@@ -0,0 +1,92 @@
+package com.nunnos.warofsuitsjoseppuit.presentation.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.nunnos.warofsuitsjoseppuit.R
+import com.nunnos.warofsuitsjoseppuit.data.Card
+import com.nunnos.warofsuitsjoseppuit.databinding.ComponentScoreboardBinding
+
+class Scoreboard @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet?
+) :
+    ConstraintLayout(context, attrs) {
+
+    private lateinit var dataBinding: ComponentScoreboardBinding
+
+    init {
+        bindView()
+        parseAttributes(attrs)
+    }
+
+    private fun bindView() {
+        dataBinding = ComponentScoreboardBinding.inflate(
+            LayoutInflater.from(context),
+            this,
+            true
+        )
+    }
+
+    private fun parseAttributes(attrs: AttributeSet?) {
+        if (attrs == null) {
+            return
+        }
+        val attributes =
+            context.obtainStyledAttributes(attrs, R.styleable.Scoreboard, 0, 0)
+        val leftTitle = attributes.getText(R.styleable.Scoreboard_SC_left_title)
+        val rightTitle = attributes.getText(R.styleable.Scoreboard_SC_right_title)
+        val leftScore = attributes.getText(R.styleable.Scoreboard_SC_left_score)
+        val rightScore = attributes.getText(R.styleable.Scoreboard_SC_right_score)
+        val suit1 = attributes.getInt(R.styleable.Scoreboard_SC_suit1_drawable, -1)
+        val suit2 = attributes.getInt(R.styleable.Scoreboard_SC_suit2_drawable, -1)
+        val suit3 = attributes.getInt(R.styleable.Scoreboard_SC_suit3_drawable, -1)
+        val suit4 = attributes.getInt(R.styleable.Scoreboard_SC_suit4_drawable, -1)
+
+        if (leftTitle != null) dataBinding.scoreboardOpponentTitle.text = leftTitle
+        if (rightTitle != null) dataBinding.scoreboardMyTitle.text = rightTitle
+        if (leftScore != null) dataBinding.scoreboardOpponentScore.text = leftScore
+        if (rightScore != null) dataBinding.scoreboardMyScore.text = rightScore
+
+        if (suit1 != -1 && suit2 != -1 && suit3 != -1 && suit4 != -1) {
+            setSuits(
+                Card.Type.values().get(suit1),
+                Card.Type.values().get(suit2),
+                Card.Type.values().get(suit3),
+                Card.Type.values().get(suit4)
+            )
+
+        }
+    }
+
+    fun setSuits(suit1: Card.Type, suit2: Card.Type, suit3: Card.Type, suit4: Card.Type) {
+        dataBinding.scoreboardSuits1.setImageResource(getResourceFromCardType(suit1))
+        dataBinding.scoreboardSuits2.setImageResource(getResourceFromCardType(suit2))
+        dataBinding.scoreboardSuits3.setImageResource(getResourceFromCardType(suit3))
+        dataBinding.scoreboardSuits4.setImageResource(getResourceFromCardType(suit4))
+    }
+
+    fun setSuits(suits: ArrayList<Card.Type>) {
+        if (suits != null && suits.size == 4) {
+            setSuits(suits[0], suits[1], suits[2], suits[3])
+        }
+    }
+
+    private fun getResourceFromCardType(type: Card.Type): Int {
+        when (type) {
+            Card.Type.CLUBS -> return R.drawable.clubs
+            Card.Type.DIAMONTS -> return R.drawable.diamonds
+            Card.Type.HEARTS -> return R.drawable.hearts
+            Card.Type.SPADES -> return R.drawable.spades
+        }
+    }
+
+    fun setOpponentScore(score: Int) {
+        dataBinding.scoreboardOpponentScore.text = score.toString()
+    }
+
+    fun setMyScore(score: Int) {
+        dataBinding.scoreboardMyScore.text = score.toString()
+    }
+}
--- app/src/main/java/com/nunnos/warofsuitsjoseppuit/presentation/feature/main/fragment/GameFragment.kt
+++ app/src/main/java/com/nunnos/warofsuitsjoseppuit/presentation/feature/main/fragment/GameFragment.kt
@@ -3,6 +3,8 @@ package com.nunnos.warofsuitsjoseppuit.presentation.feature.main.fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -11,13 +13,13 @@ import com.nunnos.warofsuitsjoseppuit.databinding.FragmentGameBinding
 import com.nunnos.warofsuitsjoseppuit.presentation.feature.main.activity.MainActivity
 import com.nunnos.warofsuitsjoseppuit.presentation.feature.main.activity.vm.MainViewModel
 
-class GameFragment() : Fragment() {
+class GameFragment : Fragment() {
     lateinit var databinding: FragmentGameBinding
     lateinit var shareViewModel: MainViewModel
 
     companion object {
         fun newInstance(): GameFragment {
-            val fragment: GameFragment = GameFragment()
+            val fragment = GameFragment()
             val args = Bundle()
             fragment.arguments = args
             return fragment
@@ -38,24 +40,48 @@ class GameFragment() : Fragment() {
     private fun setListeners() {
         databinding.gameOponentLot.setOnClickListener {
             if (shareViewModel.haveToOrganizeTheGame()) {
-                shareViewModel.dealCards()
-                shareViewModel.shuffleSuitsPriority()
-                databinding.gameFeedbackText.visibility = View.GONE
+                newGame()
             } else {
-                shareViewModel.playOneRound()
-                if (shareViewModel.isGameFinished()) {
-                    val message: String
-                    when (shareViewModel.checkIfIWonTheGame(
-                        shareViewModel.myWonDeck, shareViewModel.opponentWonDeck
-                    )) {
-                        MainViewModel.GameResult.WIN -> message = getString(R.string.you_win)
-                        MainViewModel.GameResult.LOSE -> message = getString(R.string.you_lose)
-                        MainViewModel.GameResult.TIE -> message = getString(R.string.tie)
-                    }
-                    databinding.gameFeedbackText.visibility = View.VISIBLE
-                    databinding.gameFeedbackText.text = message
-                }
+                playOneRound()
             }
         }
+    }
+
+    private fun newGame() {
+        shareViewModel.dealCards()
+        shareViewModel.shuffleSuitsPriority()
+        databinding.gameFeedbackText.visibility = GONE
+
+        databinding.gameMyLotUnder.visibility = VISIBLE
+        databinding.gameScoreBoard.visibility = VISIBLE
+        databinding.gameOponentLotUnder.visibility = VISIBLE
+
+        databinding.gameScoreBoard.setMyScore(0)
+        databinding.gameScoreBoard.setOpponentScore(0)
+        databinding.gameScoreBoard.setSuits(shareViewModel.suitPriority)
+    }
+
+    private fun playOneRound() {
+        shareViewModel.playOneRound()
+        databinding.gameScoreBoard.setMyScore(shareViewModel.myWonDeck.size)
+        databinding.gameScoreBoard.setOpponentScore(shareViewModel.opponentWonDeck.size)
+        if (shareViewModel.isGameFinished()) {
+            gameEnded()
+        }
+    }
+
+    private fun gameEnded() {
+        val message: String
+        when (shareViewModel.checkIfIWonTheGame(
+            shareViewModel.myWonDeck, shareViewModel.opponentWonDeck
+        )) {
+            MainViewModel.GameResult.WIN -> message = getString(R.string.you_win)
+            MainViewModel.GameResult.LOSE -> message = getString(R.string.you_lose)
+            MainViewModel.GameResult.TIE -> message = getString(R.string.tie)
+        }
+        databinding.gameFeedbackText.visibility = VISIBLE
+        databinding.gameFeedbackText.text = message
+        databinding.gameMyLotUnder.visibility = GONE
+        databinding.gameOponentLotUnder.visibility = GONE
     }
 }
